c29d02efed66dbb3c581bea57c4a7a53e2298f95
kabindrr <chaina_kabindra@yahoo.com>
2024-12-19 10:44:58 +1100

    global error handler

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { ResponseClient } from "./src/middlewares/ResponseClient.js";
+import { errorHandler } from "./src/middlewares/ErrorHandler.js";
 
 const app = express();
 const PORT = 8000;
+
+//middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  const message = "Server is Live";
+  ResponseClient({ req, res, message });
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, (error) => {
   error
